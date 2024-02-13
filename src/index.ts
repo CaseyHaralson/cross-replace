08988e852e99c11db6b4e3ff32ccb7ffe98ba704
Casey <casey.haralson@gmail.com>
2024-02-12 20:33:10 -0700

    adding real file and test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,40 @@
-console.log('Hello, world!');
+import {exec} from 'child_process';
+import {exit} from 'process';
+import spawn from 'cross-spawn';
+
+function sanitizeKeyForRegex(key: string) {
+  // return key.replace(/([.^$*+?{}()|\[\]\\\/\-&])/g, '\\$1');
+  return key.replace(/([.^$*+?{}()|[\]\\/\-&])/g, '\\$1');
+}
+function fillCommandWithValues(args: string[]) {
+  return args.map((arg) => {
+    Object.keys(process.env)
+      .sort((x, y) => y.length - x.length) // sort by descending length to prevent partial replacement
+      .forEach((key) => {
+        const sKey = sanitizeKeyForRegex(key);
+        console.log(sKey);
+        const regex = new RegExp(`\\$${sKey}|\${${sKey}}|%${sKey}%`, 'ig');
+        arg = arg.replace(regex, process.env[key]);
+      });
+    return arg;
+  });
+}
+
+let args = process.argv.slice(2);
+if (args.length === 1) {
+  const [command] = fillCommandWithValues(args);
+  const proc = exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    process.stdout.write(stdout);
+    process.stderr.write(stderr);
+    exit(proc.exitCode);
+  });
+} else if (args.length > 1) {
+  args = fillCommandWithValues(args);
+  const command = args.shift();
+  const proc = spawn.sync(command, args, {stdio: 'inherit'});
+  exit(proc.status);
+}
--- tests/command.test.ts
+++ tests/command.test.ts
@@ -1,3 +1,50 @@
-test('test', () => {
-  expect(1).toBe(1);
+import {execSync} from 'child_process';
+import os from 'os';
+
+function cleanOutput(output: string) {
+  const commandLineRegex = new RegExp(/^> cross-replace/);
+  const lines = output.split('\n');
+  const cleanedLines = lines.filter(
+    (line) => !commandLineRegex.test(line) && !(line.trim().length === 0)
+  );
+  return cleanedLines.join(os.EOL);
+}
+
+test('example output', async () => {
+  const expected = 'worked1 and worked2 and worked3 and worked4';
+  const output = execSync(
+    'npm run example --test1=worked1 --test2=worked2 --test3=worked3 --test4=worked4'
+  );
+  const cleanedOutput = cleanOutput(output.toString());
+  expect(cleanedOutput).toBe(expected);
+});
+
+test('example:multiple-of-same output', async () => {
+  const expected = 'worked1 and worked1 and worked1 and worked1';
+  const output = execSync('npm run example:multiple-of-same --test1=worked1');
+  const cleanedOutput = cleanOutput(output.toString());
+  expect(cleanedOutput).toBe(expected);
+});
+
+test('example:with-special-chars output', async () => {
+  const expected = 'worked1 and worked1';
+  const output = execSync(
+    'npm run example:with-special-chars --Notepad++=worked1'
+  );
+  const cleanedOutput = cleanOutput(output.toString());
+  expect(cleanedOutput).toBe(expected);
+});
+
+test('example:replacement-not-found output', async () => {
+  const expected = '$npm_config_notfound and %npm_config_notfound%';
+  const output = execSync('npm run example:replacement-not-found');
+  const cleanedOutput = cleanOutput(output.toString());
+  expect(cleanedOutput).toBe(expected);
+});
+
+test('example:empty-command output', async () => {
+  const expected = '';
+  const output = execSync('npm run example:empty-command');
+  const cleanedOutput = cleanOutput(output.toString());
+  expect(cleanedOutput).toBe(expected);
 });
